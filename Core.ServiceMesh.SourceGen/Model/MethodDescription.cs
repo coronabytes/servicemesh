eef43ddf2877ca86b653d5f40e66a34f1c58e3f8
jung <jung@corona-bytes.net>
2024-08-22 16:59:32 +0200

    -

--- Core.ServiceMesh.SourceGen/Model/MethodDescription.cs
+++ Core.ServiceMesh.SourceGen/Model/MethodDescription.cs
@@ -9,12 +9,14 @@ internal readonly record struct MethodDescription
     public readonly string Return;
     public readonly ImmutableEquatableArray<string> Parameters;
     public readonly ImmutableEquatableArray<string> Generics;
+    public readonly ImmutableEquatableArray<string> Constraints;
 
-    public MethodDescription(string name, string ret, List<string> parameter, List<string> generics)
+    public MethodDescription(string name, string ret, List<string> parameter, List<string> generics, List<string> constraints)
     {
         Name = name;
         Return = ret;
         Parameters = new(parameter);
         Generics = new(generics);
+        Constraints = new(constraints);
     }
 }
--- Core.ServiceMesh.SourceGen/ServiceMeshGenerator.cs
+++ Core.ServiceMesh.SourceGen/ServiceMeshGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using Core.ServiceMesh.SourceGen.Core;
 using Core.ServiceMesh.SourceGen.Model;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -13,10 +14,13 @@ namespace Core.ServiceMesh.SourceGen
     [Generator]
     public sealed class ServiceMeshGenerator : IIncrementalGenerator
     {
+        private const string QualifiedAttributeName = "Core.ServiceMesh.Abstractions.ServiceMeshAttribute";
+        private const string AttributeName = "ServiceMeshAttribute";
+
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
             var interfaces = context
-                .SyntaxProvider.ForAttributeWithMetadataName("Core.ServiceMesh.Abstractions.ServiceMeshAttribute", Predicate, Transform)
+                .SyntaxProvider.ForAttributeWithMetadataName(QualifiedAttributeName, Predicate, Transform)
                 .Collect();
 
             context.RegisterSourceOutput(interfaces, GenerateCode);
@@ -32,6 +36,9 @@ namespace Core.ServiceMesh.SourceGen
             var type = (ITypeSymbol)ctx.TargetSymbol;
             var serviceName = type.Name;
 
+            var attr = type.GetAttributes().Single(x => x.AttributeClass!.Name == AttributeName);
+            var serviceName2 = (string)attr.ConstructorArguments[0].Value;
+
             var methods = type.GetMembers()
                 .Where(x => x.Kind == SymbolKind.Method)
                 .OfType<IMethodSymbol>()
@@ -46,10 +53,13 @@ namespace Core.ServiceMesh.SourceGen
                 var generics = x
                     .TypeParameters.Select(arg => arg.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat))
                     .ToList();
+                var constraints = x
+                    .TypeParameters.Select(arg => arg.GetWhereStatement())
+                    .ToList();
                 var parameters = x.Parameters.Select(x => x.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)).ToList();
                 var returnType = x.ReturnType.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
 
-                return new MethodDescription(methodName, returnType, parameters, generics);
+                return new MethodDescription(methodName, returnType, parameters, generics, constraints);
             }).ToList());
         }
 
@@ -71,7 +81,8 @@ namespace Core.ServiceMesh.SourceGen
                 foreach (var m in service.Methods)
                 {
                     var code = $@"
-    public {m.Return} {m.Name}{(m.Generics.Any() ? "<" + string.Join(",", m.Generics.Select(x => x)) + ">" : string.Empty)}({string.Join(", ", m.Parameters.Select(x=>x))}) {{
+    public {m.Return} {m.Name}{(m.Generics.Any() ? "<" + string.Join(",", m.Generics.Select(x => x)) + ">" : string.Empty)}({string.Join(", ", m.Parameters.Select(x=>x))}) {(m.Constraints.Any() ? string.Join(", ", m.Constraints) : string.Empty)} {{
+        var subject = ""{service.Name}.{m.Name}.G{m.Generics.Count}P{m.Parameters.Count}"";
         return mesh.DoStuff();
     }}";
                     builder.AppendLine(code);
