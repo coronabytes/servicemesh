e3feb59c4f8cc202ba08667b76e56906c4d98e1b
jung <jung@corona-bytes.net>
2024-09-01 15:23:38 +0200

    nuget sourcegen fix?

---
+++ SampleAppNuget/Mesh/IAnotherService.cs
@@ -0,0 +1,14 @@
+﻿using Core.ServiceMesh.Abstractions;
+using SampleInterfaces;
+
+namespace SampleAppNuget.Mesh;
+
+[ServiceMesh("nugetservice")]
+public interface INugetService
+{
+    ValueTask<SampleResponse> SampleA(SampleRequest request);
+
+    ValueTask<SampleResponse> SampleB(SampleRequest request);
+
+    ValueTask SampleC(SampleRequest request);
+};
